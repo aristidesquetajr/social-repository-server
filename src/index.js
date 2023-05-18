7f4dc9806e351e2a681046b0bed621008b3ded78
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-05-18 22:07:15 +0100

    resolved: error from repository editation

--- src/index.js
+++ src/index.js
@@ -30,15 +30,25 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const props = { title, url, techs };
+
+  let updateRepository = {};
+
+  for(let index in props) {
+    if(props[index]) {
+      updateRepository[index] = props[index];
+    }
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { ...repositories[repositoryIndex], ...updateRepository };
 
   repositories[repositoryIndex] = repository;
 
